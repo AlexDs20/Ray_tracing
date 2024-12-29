20ca8672aad7660e1ec8e018b52f5fb3291337ab
Alexandre De Spiegeleer <alex-ds@live.be>
2024-12-29 23:37:37 +0100

    Can have array of spheres

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ struct Sphere {
 };
 
 struct Scene {
-    Sphere spheres;
+    Sphere* spheres;
 };
 
 void write_ppm_from_array(const f32x4* data, const Screen& screen, const char* filepath) {
@@ -46,7 +46,7 @@ void write_ppm_from_array(const f32x4* data, const Screen& screen, const char* f
         file << "\n";
     }
     file.close();
-    printf("Wrote file: %s\n", filepath);
+    printf("File written: %s\n", filepath);
 }
 
 int main() {
@@ -54,25 +54,39 @@ int main() {
 
     f32x4* result = (f32x4*)malloc(4 * screen.width * screen.height * sizeof(f32));
 
-    Scene scene;
-    scene.spheres = {{0.25f, 0.75f, 0.3f}, 0.1f};
+    const u32 N = 2;
+    Sphere spheres[N];
+    spheres[0] = {{0.25f, 0.75f, 0.3f}, 0.1f};
+    spheres[1] = {{0.65f, 0.15f, 0.3f}, 0.05f};
+    Scene scene = {
+        .spheres=&spheres[0]
+    };
+
     const f32x4 background = {0.7f, 0.3f, 0.2f, 1.0f};
     const f32x4 sphere_color = {0.2f, 0.3f, 0.6f, 1.0f};
+
 
     for (u32 h=0; h<screen.height; ++h) {
         for (u32 w=0; w<screen.width; ++w) {
             f32 x = (f32)w/screen.width;
             f32 y = (f32)h/screen.height;
-            f32 x_diff = (x-scene.spheres.x[0]);
-            f32 y_diff = (y-scene.spheres.x[1]);
-            f32 r = scene.spheres.r;
+            bool no_collision = true;
+            for (u32 n=0; n<N; n++) {
+                f32 x_diff = (x-scene.spheres[n].x[0]);
+                f32 y_diff = (y-scene.spheres[n].x[1]);
+                f32 r = scene.spheres[n].r;
 
-            if ( x_diff*x_diff + y_diff*y_diff < r*r ) {
-                result[w + h*screen.width][0] = sphere_color[0];
-                result[w + h*screen.width][1] = sphere_color[1];
-                result[w + h*screen.width][2] = sphere_color[2];
-                result[w + h*screen.width][3] = sphere_color[3];
-            } else {
+                if ( x_diff*x_diff + y_diff*y_diff < r*r ) {
+                    result[w + h*screen.width][0] = sphere_color[0];
+                    result[w + h*screen.width][1] = sphere_color[1];
+                    result[w + h*screen.width][2] = sphere_color[2];
+                    result[w + h*screen.width][3] = sphere_color[3];
+                    no_collision = false;
+                    break;
+                }
+            }
+
+            if (no_collision) {
                 // RGBA
                 result[w + h*screen.width][0] = background[0];
                 result[w + h*screen.width][1] = background[1];
